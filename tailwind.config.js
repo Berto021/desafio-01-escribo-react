ecc30ee5eae83c40f8961bc4ef6078170ca71120
Humberto <humbertolima108@gmail.com>
2023-12-01 18:08:23 -0300

    add principal colors of project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color-letter-white': '#ffffff',
+        'color-bg-principal': '#1b9dff',
+        'color-bg-button': '#93d64b',
+        'color-color-hover-button': '#f7d9bc',
+        'color-bg-hover-button': '#e58628',
+      },
+    },
   },
   plugins: [],
 };
