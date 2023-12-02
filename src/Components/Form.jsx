98e58be5e4f70d8aac9c29ac88f7c2653f389092
Humberto <humbertolima108@gmail.com>
2023-12-02 15:59:00 -0300

    fix the main function bugs

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -2,32 +2,29 @@ import React from 'react';
 import bg from '../assets/escribo-bg.png';
 import { useState } from 'react';
 export const Form = () => {
-  const [number, setNumber] = useState();
+  const [number, setNumber] = useState('');
   const [numberArray, setNumberArray] = useState([]);
   const [result, setResult] = useState();
-  const [value, setValue] = useState();
-  const [showA, setShowA] = useState([]);
+  const [showArray, setShowArray] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    escribo(number);
+    calculateNumbers(number);
   };
 
-  const escribo = (num) => {
+  const calculateNumbers = (num) => {
+    const array = [];
     setNumberArray([]);
     setNumber('');
     for (let i = num - 1; i > 0; i--) {
       if (i % 3 === 0 || i % 5 === 0) {
-        setNumberArray(numberArray.push(i));
-        console.log(numberArray);
+        array.push(i);
       }
     }
-    console.log(numberArray);
-    console.log(typeof numberArray);
-    const total = numberArray.reduce((acc, value) => acc + value, 0);
-    setResult(`Resultado do somatório: ${total}`);
-    setShowA(`Números válidos: ${numberArray}`);
-    console.log(total);
+
+    const total = array.reduce((acc, value) => acc + value, 0);
+    setResult(total);
+    setShowArray(`Números válidos: ${array}`);
   };
 
   return (
@@ -52,8 +49,8 @@ export const Form = () => {
         </button>
       </form>
       <div className='text-[1.5rem] font-bold flex justify-center items-center flex-col mt-[1.5rem]'>
-        <span className='mt-1'>{showA}</span>
-        <span className=''>{result}</span>
+        <span className='mt-1'>{showArray}</span>
+        <span className=''>{result && <>Resultado do somatório: {result}</>}</span>
       </div>
       <div className='flex justify-center items-center'>
         <img className='mt-[2rem] w-[95%]' src={bg} alt='' />
